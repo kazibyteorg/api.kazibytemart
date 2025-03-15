d7c126706c69ce13a243b5ed50062bd69ddbf55b
Zobaidul Kazi <zobaidulkazi64.official@gmail.com>
2025-03-15 13:03:13 +0600

    chore: add modules cart,order,product,inventory

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { OrderModule } from './order/order.module';
+import { CartModule } from './cart/cart.module';
+import { InventoryModule } from './inventory/inventory.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [AuthModule, UserModule, OrderModule, CartModule, InventoryModule, ProductModule],
   controllers: [AppController],
   providers: [AppService],
 })
